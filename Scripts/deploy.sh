8f8c4700e0e16f9d06308df182d116027b42f254
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-07 11:05:29 -0800

    Adding -c Release

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,4 +1,4 @@
 sh ./Scripts/install_unity.sh
-uget build -p ./Promises/Promises/Promises.csproj --config-path ./Promises/Promises/uget.config.json
-uget create -p ./Promises/Promises/Promises.csproj --config-path ./Promises/Promises/uget.config.json
-uget pack -p ./Promises/Promises/Promises.csproj --config-path ./Promises/Promises/uget.config.json
+uget build -p ./Promises/Promises/Promises.csproj -c Release --config-path ./Promises/Promises/uget.config.json
+uget create -p ./Promises/Promises/Promises.csproj -c Release --config-path ./Promises/Promises/uget.config.json
+uget pack -p ./Promises/Promises/Promises.csproj -c Release --config-path ./Promises/Promises/uget.config.json
