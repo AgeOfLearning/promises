0b5a6a265fa8f6dbcd5b0cdae701607917b3d754
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-07 15:38:08 -0800

    remove configuration

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,5 +1,5 @@
 sh ./Scripts/install_unity.sh
 cd ./Promises/Promises/
-uget build -p ./Promises.csproj --configuration=Release
-uget create -p ./Promises.csproj --configuration=Release
-uget pack -p ./Promises.csproj --configuration=Release
+uget build -p ./Promises.csproj
+uget create -p ./Promises.csproj
+uget pack -p ./Promises.csproj
