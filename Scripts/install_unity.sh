cf6b458c73f36b83568adec5e0f077eee8acee1b
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-07 09:57:04 -0800

    nuget path, skip cache

--- Scripts/install_unity.sh
+++ Scripts/install_unity.sh
@@ -5,11 +5,11 @@
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
 
 #download package if it does not already exist in cache
-if [ ! -e $UNITY_DOWNLOAD_CACHE/Unity.pkg ] ; then
+#if [ ! -e $UNITY_DOWNLOAD_CACHE/Unity.pkg ] ; then
     echo 'Downloading Unity:'
     curl --retry 5 -o $UNITY_DOWNLOAD_CACHE/Unity.pkg https://download.unity3d.com/download_unity/65e0713a5949/MacEditorInstaller/Unity-2018.2.15f1.pkg
     if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
-fi
+#fi
 
 # Run installer(s)
 echo 'Installing Unity.pkg'
