b05200f033c7fe3ed7afa773b0d14b8a00e61ea9
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-07 09:33:57 -0800

    Adding cache support for unity pkg

--- Scripts/install_unity.sh
+++ Scripts/install_unity.sh
@@ -3,10 +3,14 @@
 # Download Unity3D installer into the container
 #  The below link will need to change depending on the version, this one is for 5.5.1
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
-echo 'Downloading Unity:'
-curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/65e0713a5949/MacEditorInstaller/Unity-2018.2.15f1.pkg
-if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+
+#download package if it does not already exist in cache
+if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` ] ; then
+    echo 'Downloading Unity:'
+    curl --retry 5 -o $UNITY_DOWNLOAD_CACHE/Unity.pkg https://download.unity3d.com/download_unity/65e0713a5949/MacEditorInstaller/Unity-2018.2.15f1.pkg
+    if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+fi
 
 # Run installer(s)
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/Unity.pkg -target /
