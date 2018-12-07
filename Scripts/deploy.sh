a2390cff3cdff15d9cd5f95f8fc745fa00a25db7
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-07 09:20:55 -0800

    Fix config argument & path to install_unity

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,4 +1,4 @@
-sh ./install_unity.sh
-uget build -p ./Promises/Promises.csproj -configPath ./Promises/uget.config.json
-uget create -p ./Promises/Promises.csproj -configPath ./Promises/uget.config.json
-uget pack -p ./Promises/Promises.csproj -configPath ./Promises/uget.config.json
+sh ./Scripts/install_unity.sh
+uget build -p ./Promises/Promises.csproj -c ./Promises/uget.config.json
+uget create -p ./Promises/Promises.csproj -c ./Promises/uget.config.json
+uget pack -p ./Promises/Promises.csproj -c ./Promises/uget.config.json
