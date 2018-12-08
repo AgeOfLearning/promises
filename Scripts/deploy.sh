304d8672e016d6bf815e61785ef5a7a4812ac6c9
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-07 18:07:31 -0800

    ls

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,5 +1,7 @@
 sh ./Scripts/install_unity.sh
 cd ./Promises/Promises/
+ls /Applications/Unity
+ls /Applications/Unity/Unity.app
 uget build -p ./Promises.csproj --debug
 uget create -p ./Promises.csproj --debug
 uget pack -p ./Promises.csproj --debug
