d617487af907639410be4acddd885a9fbb73f9bf
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-12-10 10:29:19 -0800

    fix python command

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -2,7 +2,7 @@ sh ./Scripts/install_unity.sh
 cd ./Promises/Promises/
 ls /Applications/Unity
 ls /Applications/Unity/Unity.app
-python -c "import os; print(os.path.is_file('/Applications/Unity/Unity.app/Contents/MacOS/Unity'))"
+python -c "import os; print(os.path.isfile('/Applications/Unity/Unity.app/Contents/MacOS/Unity'))"
 uget build -p ./Promises.csproj --debug
 uget create -p ./Promises.csproj --debug
 uget pack -p ./Promises.csproj --debug
